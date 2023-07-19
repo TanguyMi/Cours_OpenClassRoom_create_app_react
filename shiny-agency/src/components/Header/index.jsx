af5034effd91c8d633c383471fe2fc818fbfca0d
tanguyMi <tanguy.michel78@gmail.com>
2023-07-19 22:06:39 +0200

    l'exo 3 est finie

--- shiny-agency/src/components/Header/index.jsx
+++ shiny-agency/src/components/Header/index.jsx
@@ -1,13 +1,48 @@
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import colors from '../../utils/style/color';
+import Logo from '../../assets/dark-logo.png';
+
+const StyledLink = styled(Link)`
+    padding: 10px 30px;
+    color: #8186a0;
+    text-decoration: none;
+    font-size: 18px;
+    ${(props) =>
+      props.$isFullLink &&
+      `color: white; border-radius: 30px; background-color: ${colors.primary};`}
+`;
+const HeaderContainer = styled.header`
+    height: 100px;
+    display: flex;
+    align-content:center;
+    align-items:center;
+    position:relative
+`;
+
+const Nav = styled.nav`
+   position:absolute;
+   right:0
+`;
+const LogoHeader = styled.img`
+     position:relative;
+   left:30px;
+   bottom: 0px;
+   width: 187.629px;
+      height: 70px;
+      flex-shrink: 0;
+`;
 
 function Header() {
       return (
-            <nav>
-                  <Link to='/'>Accueil</Link>
-                  <Link to='/Results'>Results</Link>
-                  <Link to='/Freelances'>Freelances</Link>
-                  <Link to='/survey/1'>Questionnaire</Link>
-            </nav>
+            <HeaderContainer>
+                  <LogoHeader src={Logo} alt='Logo'></LogoHeader>
+                  <Nav>
+                        <StyledLink to='/'>Accueil</StyledLink>
+                        <StyledLink to='/freelances'>Profils</StyledLink>
+                        <StyledLink to='/survey/1' $isFullLink>Faire le test</StyledLink>
+                  </Nav>
+            </HeaderContainer>
       );
 }
 
